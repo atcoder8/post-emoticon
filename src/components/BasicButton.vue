c245b80ef8cfbf0f7d25690c01712229a83eeed9
pencoder <atcoder8@gmail.com>
2025-11-09 15:48:43 +0900

    基本的な機能を持つボタンコンポーネントを追加しました。

---
+++ src/components/BasicButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  /** ボタンのラベル */
+  label: string;
+
+  /** ボタンを有効化するかどうかを指定するフラグ */
+  enabled: boolean;
+}
+
+defineProps<Props>();
+
+defineEmits<{ click: [] }>();
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="$emit('click')"
+    :disabled="!enabled"
+    class="px-2 py-1 border rounded-lg"
+    :class="enabled ? 'cursor-pointer hover:bg-blue-100 active:bg-blue-300' : 'text-gray-300'"
+  >
+    {{ label }}
+  </button>
+</template>
